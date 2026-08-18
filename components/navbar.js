157b67167e43235b6e4c5500646775ec757ca25f
Jackson Dickison <jaksondikison@gmail.com>
2026-08-17 20:58:26 -0700

    Style navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,39 @@
 import { html } from "aspen";
 
+export function Link({ href, children }) {
+  return html`<a
+    style="
+      color: #589966; 
+      text-decoration: none; 
+      font-size: 16px;
+      font-family: Inter;
+      letter-spacing: 3%;
+    "
+    href=${href}
+    >${children}</a
+  >`;
+}
+
 export function Navbar() {
   return html`<div
     style="
       display: flex; 
       flex-direction: row; 
-      gap: 12px; 
+      gap: 24px; 
       justify-content: center;
     "
   >
-    <a>Quick start</a>
-    <a>API reference</a>
-    <a>X.com</a>
+    <Link href="https://github.com/aspen-js/core#quick-start">Quick start</Link>
+    <Link href="https://github.com/aspen-js/core#api-reference">API reference</Link>
+    <Link href="https://x.com">X.com</Link>
+    <a class="github-button"
+      href="https://github.com/aspen-js/core"
+      data-color-scheme="no-preference: light; light: light; dark: dark;" 
+      data-icon="octicon-star" 
+      data-show-count="true" 
+      aria-label="Star aspen-js/core on GitHub"
+    >
+      Star
+    </a>
   </div>`;
 }
