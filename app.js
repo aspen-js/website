f80477c8f5ca7a1af2aada3d4e7d41cc0f8f5107
Jackson Dickison <jaksondikison@gmail.com>
2026-08-19 05:54:47 -0700

    Add Aspen logo

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import { html } from "aspen";
 
 export * as Navbar from "./components/navbar.js";
+export * as AspenLogo from "./components/aspen-logo.js";
 
 export function App() {
-  return html`<Navbar />`;
+  return html`
+    <Navbar />
+    <div
+      style="
+        display: flex;
+        justify-content: center;
+        margin-inline: auto;
+      "
+    >
+      <AspenLogo />
+    </div>
+  `;
 }
