f6f3e9517633e129eadf8017568afdbbd4e1b863
Jackson Dickison <jaksondikison@gmail.com>
2026-08-21 07:49:05 -0700

    Add mobile layout for button and logo

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import { html } from "aspen";
 
+import { $isMobile } from "./layout.js";
+
 export * as Navbar from "./components/navbar.js";
 export * as AspenLogo from "./components/aspen-logo.js";
 export * as ButtonLink from "./components/button-link.js";
@@ -8,14 +10,15 @@ export function App() {
   return html`
     <Navbar />
     <div
-      style="
+      style=${`
         display: flex;
+        flex-direction: ${$isMobile.val ? "column" : "row"};
         justify-content: center;
         align-items: center;
         margin-inline: auto;
-        gap: 174px;
+        gap: ${$isMobile.val ? 74 : 174}px;
         margin-top: 74px;
-      "
+      `}
     >
       <AspenLogo />
       <ButtonLink href="https://github.com/aspen-js/core#aspen">
