c17391decbc464689fd66a04fd5dc45d22f9fe3c
Jackson Dickison <jaksondikison@gmail.com>
2026-09-04 18:00:56 -0700

    Remove unnecessary task call

--- layout.js
+++ layout.js
@@ -1,11 +1,9 @@
-import { task, signal } from "aspen";
+import { signal } from "aspen";
 
 const mediaQuery = matchMedia("(width <= 640px)");
 
 export const $isMobile = signal(mediaQuery.matches);
 
-task(() => {
-  mediaQuery.addEventListener("change", () => {
-    $isMobile.val = mediaQuery.matches;
-  });
+mediaQuery.addEventListener("change", () => {
+  $isMobile.val = mediaQuery.matches;
 });
